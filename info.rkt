620a52ed612de99c867c9dda8996569dad868214
DarrenN <info@v25media.com>
2017-05-13 20:31:55 -0400

    Bump version to 0.1.0

--- info.rkt
+++ info.rkt
@@ -6,5 +6,5 @@
 (define build-deps '("scribble-lib" "racket-doc"))
 (define scribblings '(("scribblings/simple-http.scrbl" ())))
 (define pkg-desc "Simple interface for making HTTP requests")
-(define version "0.1")
+(define version "0.2.0")
 (define pkg-authors '(Darren_N))
